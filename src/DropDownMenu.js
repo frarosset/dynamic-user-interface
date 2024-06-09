f7a73425e62a596a924f8634ac408f2de75f8433
frarosset <37275581+frarosset@users.noreply.github.com>
2024-06-09 11:44:51 +0200

    Add DropDownMenu method to initialize the menu items
    When creating a new DropDownMenu, you has to pass an array with
    info about the dropdown menu items. The items of this array are
    object with a label and a link field. A null field, means that the
    item is a simple label. Otherwise, an <a> element is created,
    pointing to the 'link' field.
    
    let menuData = [
        {"label": "Anchor 1", "link":"#"},
        {"label": "Anchor 2", "link":"#"},
        {"label": "Simple Label", "link":null}
    ];

--- src/DropDownMenu.js
+++ src/DropDownMenu.js
@@ -1,19 +1,49 @@
 import './DropDownMenu.css';
 
 export default class DropDownMenu{
-    constructor(parentDiv){
-        let dropDownDiv = document.createElement('div');
-        dropDownDiv.classList.add('drop-down-menu');
+    #dropDownDiv;
+    #button;
+    #content;
+    #items = [];
 
-        let button = document.createElement('button');
-        button.classList.add('drop-down-button');
-        button.textContent = '=';
+    constructor(parentDiv, menuData){
+        this.#dropDownDiv = document.createElement('div');
+        this.#dropDownDiv.classList.add('drop-down-menu');
 
-        let content = document.createElement('ul');
-        content.classList.add('drop-down-content');
+        this.#button = document.createElement('button');
+        this.#button.classList.add('drop-down-button');
+        this.#button.textContent = '=';
 
-        dropDownDiv.appendChild(button);
-        dropDownDiv.appendChild(content);
-        parentDiv.appendChild(dropDownDiv);
+        this.#content = document.createElement('ul');
+        this.#content.classList.add('drop-down-content');
+
+        menuData.forEach(data => {
+            console.log(data);
+            this.addItem(data);
+        });
+
+        this.#dropDownDiv.appendChild(this.#button);
+        this.#dropDownDiv.appendChild(this.#content);
+        parentDiv.appendChild(this.#dropDownDiv);
+    }
+
+    addItem(data){
+        // itmType: button, anchor
+        let item = document.createElement('li');
+
+        if (data.link){
+            let anchor = document.createElement('a');
+            anchor.href = data.link;
+            //anchor.target = '_blank'; // Opens link in a new tab
+            anchor.textContent = data.label;
+
+            item.classList.add('anchor');
+            item.appendChild(anchor);
+        } else {
+            item.textContent = data.label;
+        }
+
+        this.#items.push(item);
+        this.#content.appendChild(item);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import DropDownMenu from './DropDownMenu.js';
 
+let menuData = [
+    {"label": "Anchor 1", "link":"#"},
+    {"label": "Anchor 2", "link":"#"},
+    {"label": "Simple Label", "link":null}
+];
+
 function init(){
     let body = document.querySelector('body');
 
     let header = document.createElement('header');
     body.appendChild(header);
     
-    let dropDownMenu = new DropDownMenu(header);
+    let dropDownMenu = new DropDownMenu(header,menuData);
 
 }
 
