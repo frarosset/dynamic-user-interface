80104ab9c60498c9650cdffdc92354d976c362be
frarosset <37275581+frarosset@users.noreply.github.com>
2024-06-11 12:26:39 +0200

    Add method to show a specific slide in the image carousel
    Add #showSlide(idx) method to the ImageCarousel class to shown in
    the frame the slide/image at index idx.
    
    idx is assumed to be a valid index (between 0 and N-1. where N is
    the total number of images).
    
    To do the actual switch, the .image-carousel-slides element
    containing all the slides (side by side, horizontally) has been set
    to position relative, so that you can change the 'left' property
    to move this div within the frame.
    
    As all the slides/images have the same width (100% of the frame),
    to show the image at index idx, you have to set the 'left' property
    to `-${idx*100}%` (eg, for the image at index 4, you need to set
    "left:'-400%'").

--- src/ImageCarousel.js
+++ src/ImageCarousel.js
@@ -3,6 +3,7 @@ import './ImageCarousel.css';
 export default class ImageCarousel{
     #imageCarouselDiv;
     #imgs = [];
+    #imagesDiv;
 
     constructor(parentDiv,imagesPaths){
         this.#imageCarouselDiv = document.createElement('div');
@@ -11,21 +12,26 @@ export default class ImageCarousel{
         const frameDiv = document.createElement('div');
         frameDiv.classList.add('image-carousel-frame');
         
-        const imagesDiv = document.createElement('div');
-        imagesDiv.classList.add('image-carousel-slides');
+        this.#imagesDiv = document.createElement('div');
+        this.#imagesDiv.classList.add('image-carousel-slides');
         imagesPaths.forEach(imgPath => {
             const img = document.createElement('img');
             img.setAttribute('src',imgPath);
 
             this.#imgs.push(img);
-            imagesDiv.appendChild(img);
+            this.#imagesDiv.appendChild(img);
         });
 
-        frameDiv.appendChild(imagesDiv);
+        frameDiv.appendChild(this.#imagesDiv);
         this.#imageCarouselDiv.appendChild(frameDiv);
 
         console.log(this.#imgs);
-        
+        this.#showSlide(1);
+
         parentDiv.appendChild(this.#imageCarouselDiv);
+    }
+
+    #showSlide(idx=0){
+        this.#imagesDiv.style.left = `-${idx*100}%`;
     }
 }
