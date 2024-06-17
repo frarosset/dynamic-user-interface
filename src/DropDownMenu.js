a6407636f691ed042e233dc30289c79ffed13101
frarosset <37275581+frarosset@users.noreply.github.com>
2024-06-17 11:29:49 +0200

    Refactor DropDownMenu component to use BEM for css class naming

--- src/DropDownMenu.js
+++ src/DropDownMenu.js
@@ -1,26 +1,29 @@
 import './DropDownMenu.css';
 import {initDiv, initP, initButton, initA, initLi, initUl, initHr} from './commonDomComponents.js';
 
+let blockName = 'drop-down-menu'; // ddm = drop-down-menu
+
 export default class DropDownMenu{
     #dropDownDiv;
     #button;
     #content;
+    #contentCnt;
 
     constructor(parentDiv, menuData, enableHover=false, btnFaIcon = {prefix: 'solid', icon: 'bars'}, btnLabel='', contentPosition){
         if (contentPosition !== 'left' && contentPosition !== 'right')
             contentPosition = 'center';
-        this.#dropDownDiv = initDiv(['drop-down-menu', `content-align-${contentPosition}`]);
-        this.#button = initButton('drop-down-button', this.#buttonClickCallback, btnFaIcon, btnLabel);
-        let contentCnt = initDiv('drop-down-content-cnt');
-        this.#content = initUl('drop-down-content');
+        this.#dropDownDiv = initDiv(blockName);
+        this.#button = initButton(`${blockName}__button`, this.#buttonClickCallback, btnFaIcon, btnLabel);
+        this.#contentCnt = initDiv([`${blockName}__content-container`, `${blockName}__content-container--align--${contentPosition}`]);
+        this.#content = initUl(`${blockName}__content`);
 
         menuData.forEach(data => {
             this.addItem(data);
         });
 
         this.#dropDownDiv.appendChild(this.#button);
-        contentCnt.appendChild(this.#content);
-        this.#dropDownDiv.appendChild(contentCnt);
+        this.#contentCnt.appendChild(this.#content);
+        this.#dropDownDiv.appendChild(this.#contentCnt);
         parentDiv.appendChild(this.#dropDownDiv);
 
         this.#content.addEventListener('click', this.#contentClickCallback);
@@ -32,35 +35,31 @@ export default class DropDownMenu{
 
     addItem(data){
         if (data.label === null){ // separator
-            const separator = initHr('separator');
+            const separator = initHr(`${blockName}__separator`);
             this.#content.appendChild(separator);
             return;
         }
 
-        let liClass;
         let liChild;
 
         if (data.link){ // anchor
-            liClass = 'anchor';
-            liChild = initA('', data.link, data.faIcon,data.label);
+            liChild = initA(`${blockName}__anchor`, data.link, data.faIcon,data.label);
         } else if (data.action){ // action
-            liClass = 'action';
-            liChild = initButton('', data.action, data.faIcon, data.label);
+            liChild = initButton(`${blockName}__action`, data.action, data.faIcon, data.label);
         } else { // label
-            liClass = 'label';
-            liChild = initP('', data.faIcon,data.label);
+            liChild = initP(`${blockName}__label`, data.faIcon,data.label);
         }
 
-        const li = initLi(this.#content, liClass);
+        const li = initLi(this.#content, `${blockName}__item`);
         li.appendChild(liChild);
     }
 
     toggleVisibility(condition=undefined){
-        this.#dropDownDiv.classList.toggle('visible',condition);
+        this.#contentCnt.classList.toggle(`${blockName}__content-container--visible`,condition);
     }
 
     toggleForcedVisibility(condition=undefined){
-        this.#dropDownDiv.classList.toggle('visible-forced',condition);
+        this.#contentCnt.classList.toggle(`${blockName}__content-container--visible-forced`,condition);
     }
  
     // Event listeners callbacks ----------------------------------------------
